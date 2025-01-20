9eb08eba66d3d7b94fc1c6c624f53c1ed9a91068
meghanas086 <meghana.s@robosoftin.com>
2025-01-20 13:45:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ mongoose
     console.error(MESSAGES.DB_ERROR, error);
   });
 
-app.listen(PORT,'0.0.0.0' () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
